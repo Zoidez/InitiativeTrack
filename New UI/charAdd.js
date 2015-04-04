f83cea2baf13711a01ac468632ee4e0616384359
Dima <dima.buchka@gmail.com>
2015-04-04 00:26:48 -0300

    new UI 0.4
    The design of charAddForm is finally settled. Now, I need to finish
    charForm object blueprint and make it functional first before i proceed
    to making more charForms
    -finish charForm
    -make charForm (initial) functional
    -make more charForms

--- New UI/charAdd.js
+++ New UI/charAdd.js
@@ -1,3 +1,29 @@
+function charAdd() {
+	
+	//-----------Creating the HTML fields-----------
+	this.form = document.createElement("form");
+	this.name = document.createElement("input");
+	this.pc = document.createElement("button");
+	this.npc = document.createElement("button");
+	this.baseInit = document.createElement("input");
+	this.d6Init = document.createElement("input");
+	this.woundMod = document.createElement("select");
+	this.option0 = document.createElement("option");
+	this.option1 = document.createElement("option");
+	this.option2 = document.createElement("option");
+	this.option3 = document.createElement("option");
+	this.option4 = document.createElement("option");
+	this.option5 = document.createElement("option");
+	this.option6 = document.createElement("option");
+	this.option7 = document.createElement("option");
+	this.option8 = document.createElement("option");
+	this.option9 = document.createElement("option");
+	this.notes = document.createElement("input");
+	
+	//-----------Assigning the classes and ids to the elements-----------
+	this.form
+}
+
 function pcNpcButtonOnClick(isPc) {
 	var pcButton = document.getElementById("charPcButton");
 	var npcButton = document.getElementById("charNpcButton");
